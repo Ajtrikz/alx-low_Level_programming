6c918ed96b27cb247aaea08d0a3ea05c4508bdb4
Ajtrikz <ajtrikzconsortium@gmail.com>
2022-09-16 09:29:09 -0700

    This is my first C program using puts function to print a string of text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	//string initialization 
+	char puzzle[] = "Programming is like building a multilingual puzzle\n";
+
+	//writing the string to stdout
+	puts(puzzle); 
+
+	return 0;
+}
+
