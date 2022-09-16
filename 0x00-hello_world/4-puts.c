403ae5c74b019c25675e62b31f3fa7514463c02b
Ajtrikz <ajtrikzconsortium@gmail.com>
2022-09-16 10:04:31 -0700

    Now it compiles correctly with betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,9 +9,8 @@
  */
 int main(void)
 {
-	 char puzzle[] = "\"Programming is like building a multilingual puzzle"; 
-
+	 char puzzle[] = "\"Programming is like building a multilingual puzzle";
 	 puts(puzzle);
-	return 0;
+	return (0);
 }
 
